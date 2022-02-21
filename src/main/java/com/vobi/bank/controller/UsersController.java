cdde08623d687bb8007b7e5780276bdf8f8ffe4b
Cristian Franco <cfrancobedoya@gmail.com>
2022-02-20 20:42:10 -0500

    Add PUT method

--- src/main/java/com/vobi/bank/controller/UsersController.java
+++ src/main/java/com/vobi/bank/controller/UsersController.java
@@ -1,11 +1,17 @@
 package com.vobi.bank.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vobi.bank.domain.Users;
+import com.vobi.bank.dto.UsersDTO;
 import com.vobi.bank.mapper.UsersMapper;
 import com.vobi.bank.service.UsersService;
 
@@ -22,5 +28,14 @@ public class UsersController {
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable("id") String id) throws Exception {
 		usersService.deleteById(id);
+	}
+
+	@PutMapping
+	public UsersDTO update(@Valid @RequestBody UsersDTO usersDTO) throws Exception {
+		Users user = usersMapper.usersDTOtoUsers(usersDTO);
+		user = usersService.update(user);
+		usersDTO = usersMapper.usersToUsersDTO(user);
+
+		return usersDTO;
 	}
 }
