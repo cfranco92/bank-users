d3a7e36345523f0e64768b1f1b4c3ad766ff6d55
Cristian Franco <cfrancobedoya@gmail.com>
2022-02-20 20:53:44 -0500

    Add GET, DELETE, PUT

--- src/main/java/com/vobi/bank/controller/UsersController.java
+++ src/main/java/com/vobi/bank/controller/UsersController.java
@@ -1,10 +1,14 @@
 package com.vobi.bank.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +41,36 @@ public class UsersController {
 		usersDTO = usersMapper.usersToUsersDTO(user);
 
 		return usersDTO;
+	}
+
+	@PostMapping
+	public UsersDTO save(@Valid @RequestBody UsersDTO usersDTO) throws Exception {
+		Users user = usersMapper.usersDTOtoUsers(usersDTO);
+		user = usersService.save(user);
+		usersDTO = usersMapper.usersToUsersDTO(user);
+
+		return usersDTO;
+	}
+
+	@GetMapping("/{id}")
+	public UsersDTO findById(@PathVariable("id") String id) throws Exception {
+
+		Users user = null;
+		UsersDTO usersDTO = null;
+		if (usersService.findById(id).isPresent() == true)
+			user = usersService.findById(id).get();
+
+		usersDTO = usersMapper.usersToUsersDTO(user);
+
+		return usersDTO;
+	}
+
+	@GetMapping()
+	public List<UsersDTO> findAll() throws Exception {
+
+		List<Users> users = usersService.findAll();
+		List<UsersDTO> usersDTOs = usersMapper.usersListToUsersDTOList(users);
+
+		return usersDTOs;
 	}
 }
